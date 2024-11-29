b7269969f50ec384b816efde4735227df4a356e3
Shubham-Pareta <ishubhampareta@gmail.com>
2024-11-29 13:11:31 +0530

    Changed app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from dotenv import load_dotenv
-load_dotenv()
 import streamlit as st
 import os 
 import google.generativeai as genai
+
+load_dotenv()
+
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
 
 model=genai.GenerativeModel("gemini-pro")
 def my_output(query):
