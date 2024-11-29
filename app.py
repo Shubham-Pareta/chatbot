0740a92f68eddd349316cbd0cecb331d664ceb73
Shubham-Pareta <ishubhampareta@gmail.com>
2024-11-29 13:14:40 +0530

    Changed app.py

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 from dotenv import load_dotenv
-import streamlit as st
-import os 
-import google.generativeai as genai
-
 load_dotenv()
-
+import streamlit as st
+import os
+import google.generativeai as genai
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
+#model = genai.GenerativModel("gemini-flash-1.5")
+model = genai.GenerativeModel("gemini-pro")
 
-model=genai.GenerativeModel("gemini-pro")
 def my_output(query):
-    response = model.generate_content(query)
+    response= model.generate_content(query)
     return response.text
 
-# UI Development using Streamlit
+## UI Development using stramlit
 
 st.set_page_config(page_title="SMART_BOT")
 st.header("SMART_BOT")
@@ -22,5 +21,5 @@ submit=st.button("Ask Your Query")
 
 if submit:
     response=my_output(input)
-    st.subheader("Output :")
+    st.subheader("The Response is:")
     st.write(response)
